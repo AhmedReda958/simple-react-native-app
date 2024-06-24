ac7c07ce2fe96c23478353c34ec5c2d0fbbdbb9b
AhmedReda958 <areda.gad@gmail.com>
2024-06-24 03:12:36 +0300

    use hooks And network requests

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -1,93 +1,72 @@
+import { useCallback, useEffect, useState } from "react";
 import {
-  Button,
   Text,
   View,
   StyleSheet,
-  SectionList,
   FlatList,
   TouchableOpacity,
 } from "react-native";
 
-const COLORS = [
-  { colorName: "Base03", hexCode: "#002b36" },
-  { colorName: "Base02", hexCode: "#073642" },
-  { colorName: "Base01", hexCode: "#586e75" },
-  { colorName: "Base00", hexCode: "#657b83" },
-  { colorName: "Base0", hexCode: "#839496" },
-  { colorName: "Base1", hexCode: "#93a1a1" },
-  { colorName: "Base2", hexCode: "#eee8d5" },
-  { colorName: "Base3", hexCode: "#fdf6e3" },
-];
+const PalettePreview = ({ list, navigation }) => (
+  <View key={list.title}>
+    <Text style={styles.sectionTitle}>{list.paletteName}</Text>
 
-const RAINBOW = [
-  { colorName: "Yellow", hexCode: "#b58900" },
-  { colorName: "Orange", hexCode: "#cb4b16" },
-  { colorName: "Red", hexCode: "#dc322f" },
-  { colorName: "Magenta", hexCode: "#d33682" },
-  { colorName: "Violet", hexCode: "#6c71c4" },
-  { colorName: "Blue", hexCode: "#268bd2" },
-  { colorName: "Cyan", hexCode: "#2aa198" },
-  { colorName: "Green", hexCode: "#859900" },
-];
-const FRONTEND_MASTERS = [
-  { colorName: "Red", hexCode: "#c02d28" },
-  { colorName: "Black", hexCode: "#3e3e3e" },
-  { colorName: "Grey", hexCode: "#8a8a8a" },
-  { colorName: "White", hexCode: "#ffffff" },
-  { colorName: "Orange", hexCode: "#e66225" },
-];
-
-const colors = [
-  { title: "Solarized", data: COLORS },
-  { title: "Rainbow", data: RAINBOW },
-  { title: "Frontend Masters", data: FRONTEND_MASTERS },
-];
+    <TouchableOpacity
+      onPress={() =>
+        navigation.push("ColorsList", {
+          paletteName: list.paletteName,
+          colors: list.colors,
+        })
+      }
+    >
+      <FlatList
+        data={list.colors}
+        keyExtractor={(item) => item.hexCode}
+        renderItem={({ item }) => (
+          <View
+            style={[styles.colorItem, { backgroundColor: item.hexCode }]}
+          ></View>
+        )}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionTitle}>{section.title}</Text>
+        )}
+        horizontal
+      />
+    </TouchableOpacity>
+  </View>
+);
 
 const Home = ({ navigation }) => {
+  const [colors, setColors] = useState([]);
+
+  const fetchColors = useCallback(async () => {
+    const result = await fetch(
+      "https://color-palette-api.kadikraman.vercel.app/palettes"
+    );
+    const palettes = await result.json();
+    if (palettes) {
+      setColors(palettes);
+    }
+  }, []);
+
+  useEffect(() => fetchColors(), []);
+
   return (
-    <View style={styles.container}>
-      {colors.map((list) => (
-        <View key={list.title}>
-          <Text style={styles.sectionTitle}>{list.title}</Text>
-
-          <TouchableOpacity
-            onPress={() =>
-              navigation.push("ColorsList", {
-                paletteName: list.title,
-                colors: list.data,
-              })
-            }
-          >
-            <FlatList
-              data={list.data}
-              keyExtractor={(item) => item.hexCode}
-              renderItem={({ item }) => (
-                <View
-                  style={[styles.colorItem, { backgroundColor: item.hexCode }]}
-                ></View>
-              )}
-              renderSectionHeader={({ section }) => (
-                <Text style={styles.sectionTitle}>{section.title}</Text>
-              )}
-              horizontal
-            />
-          </TouchableOpacity>
-        </View>
-      ))}
-
-      {/* <Button
-        onPress={() => navigation.push("ColorsList")}
-        color={"gray"}
-        title="go to color list"
-      /> */}
-    </View>
+    <FlatList
+      style={styles.container}
+      data={colors}
+      renderItem={({ item }) => (
+        <PalettePreview list={item} navigation={navigation} />
+      )}
+      ListEmptyComponent={<Text>Loading...</Text>}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 16,
+    paddingHorizontal: 10,
   },
   colorItem: {
     height: 50,
